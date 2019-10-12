80ffe2726cbd255bd61efc6589934b2bb96d6afc
Archie <archiegod@gmail.com>
2019-10-12 19:09:41 +0100

    Moved bootstrap link

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ import Page2 from './pages/Page2.js';
 export default function App() {
   return (
     <Router>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
       <Header />
       <div style={{
         margin: `0 auto`,
