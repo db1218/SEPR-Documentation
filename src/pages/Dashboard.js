5da46fb54660dca31582514981460f22018f8eee
Archie <archiegod@gmail.com>
2019-11-03 16:13:06 +0000

    Fixed warnings

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "../layout.css"
-import Link from "react-router-dom/Link";
+import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button'
 
 export default class Dashboard extends React.Component {
@@ -8,7 +8,7 @@ export default class Dashboard extends React.Component {
         return (
            <div>
                <h1>Welcome to Team 11's documentation site</h1>
-               <p1>Over time we will add details about our project</p1>
+               <p>Over time we will add details about our project</p>
                <div>
                 <Link to="/page2">
                     <Button>Page-2</Button>
